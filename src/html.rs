f62c7c0aba28783e8aaca5f969d3f34bb11b80bb
Michael Eberhardt <github@michael-eberhardt.de>
2022-09-25 18:12:50 +0300

    Refactored everything for a more-generic approach

--- src/html.rs
+++ src/html.rs
@@ -4,14 +4,8 @@ use super::writer::*;
 /// The Writer struct/class, to be used to fill the content-string with HTML.
 #[derive(Debug, Clone)]
 pub struct HTMLWriter {
-    // holds the whole file content as long the Writer is used
-    pub content: String,
-    // number of whitespaces one indent-step means
-    indent_step_size: usize,
-    // holds the current indent as a string for quick adding into content
-    indent: String,
-    // holds a stack with opened/unclosed block-tags
-    block_stack: Vec<String>
+    /// WriterCore in a composition
+    pub core: WriterCore
 }
 
 
@@ -19,104 +13,64 @@ impl MLLWriter for HTMLWriter {
     // Type declaration
     type MLLWriter = HTMLWriter;
 
-    /// Method generates a new Writer with empty content and presets, e.g. zero indent
+    
     fn new() -> HTMLWriter {
-        HTMLWriter{
-            content: String::new(),
-            indent_step_size: 4,
-            indent: String::new(),
-            block_stack: Vec::new(),
-        }
+        HTMLWriter { core: WriterCore::new() }
     }
 
-    /// Method resets the HTMLWriter to defaults and empties the content-string as well
-    fn clear(&mut self) {
-        self.content.clear();
-        self.set_indent_step(0);
-        self.set_indent_step_size(4);
-        self.block_stack.clear();
-    }
-
-    /// Method opens a new block, e.g. <div> tag
+    
     fn w_open_element(&mut self, tag: &str) {
-        self.content.push_str(&["<".to_string() + tag + ">"].concat());
-        self.block_stack.push(tag.to_string());
+        self.core.content.push_str(&["<".to_string() + tag + ">"].concat());
+        self.core.block_stack.push(tag.to_string());
     }
 
-    /// Method closes the last opened block, e.g. </div> tag.
+    
     fn w_close_element(&mut self) {
-        let tag = self.block_stack.pop().unwrap();
-        self.content.push_str(&["</".to_string() + &tag + ">"].concat());
+        let tag = self.core.block_stack.pop().unwrap();
+        self.core.content.push_str(&["</".to_string() + &tag + ">"].concat());
     }
 
-    /// Method prints a single-tag element into the content-string
+    
     fn w_single_element(&mut self, tag: &str) {
-        self.content.push_str(&["<".to_string() + tag + ">"].concat());
+        self.core.content.push_str(&["<".to_string() + tag + ">"].concat());
     }
 
-    /// Method adds a single property-value-pair and pushes it onto the content-string retroactively.
+    
     fn w_property(&mut self, name: &str, value: &str) {
         // First we remove the '>' of the last entry
-        self.content.pop();
+        self.core.content.pop();
         // Then add the property-value-pair and close the tag again after insertion
-        self.content.push_str(&[" ".to_string() + name + "=\"" + value + "\">"].concat());
+        self.core.content.push_str(&[" ".to_string() + name + "=\"" + value + "\">"].concat());
     }
 
-    /// Method generates a property-string out of given properties and pushes it onto content-string retroactively.
-    /// It uses therefor the Property-struct definition to be able to accept an arbitrary number of properties.
+    
     fn w_properties(&mut self, properties: &Property) {
         // First we remove the '>' of the last entry
-        self.content.pop();
+        self.core.content.pop();
         // Then, we add our property-string
-        self.content.push_str(&[properties.html_str() + ">"].concat());
+        properties.p.iter().for_each(|x| self.core.content.push_str(
+            &(" ".to_string() + &x.0 + "=\"" + &x.1 + "\"")
+        ));
+        // Finally, we close the tag again
+        self.core.content.push_str(">");
     }
 
-    /// Method adds a line feed to content string and writes the current indent
-    fn w_lf(&mut self) {
-        self.content.push_str(&["\n".to_string() + &self.indent].concat());
-    }
+    
+    fn clear(&mut self) { self.core.clear(); }
 
-    /// Method meaningful combines inc_indent_step() and w_lf() 
-    fn w_lf_inc(&mut self) {
-        self.inc_indent_step();
-        self.w_lf();
-    }
+    fn w_lf(&mut self) { self.core.w_lf(); }
+    
+    fn w_lf_inc(&mut self) { self.core.w_lf_inc(); }
 
-    /// Method meaningful combines dec_indent_step() and w_lf() 
-    fn w_lf_dec(&mut self) {
-        self.dec_indent_step();
-        self.w_lf();
-    }
+    fn w_lf_dec(&mut self) { self.core.w_lf_dec(); }
+    
+    fn inc_indent_step(&mut self) { self.core.inc_indent_step(); }
 
-    /// Method increases the current indent by indent_step_size
-    fn inc_indent_step(&mut self) {
-        self.indent.push_str(" ".repeat(self.indent_step_size).as_str());
-    }
+    fn dec_indent_step(&mut self) { self.core.dec_indent_step(); }
 
-    /// Method decreases the current indent by indent_step_size
-    fn dec_indent_step(&mut self) {
-        let steps = if self.indent_step_size > self.indent.len() { 
-            self.indent.len() 
-        }
-        else { 
-            self.indent_step_size 
-        };
-        for _i in 0..steps {
-            self.indent.pop();
-        }
-    }
+    fn set_indent_step(&mut self, indent_step: usize) { self.core.set_indent_step(indent_step); }
 
-    /// Set an arbitrary certain indent step. The method automatically multiplies the given value with current indent_step_size
-    /// and resets an internal string for faster inserting the current indent during the document generation progress.
-    fn set_indent_step(&mut self, indent_step: usize) {
-        self.indent = " ".repeat(indent_step * self.indent_step_size).to_string();
-    }
-
-    /// Set the indent-step-size (the number of whitespaces per indent-step). Default is 4 whitespaces. Method results an Err if
-    /// called after started editing (content isn't empty anymore).
-    fn set_indent_step_size(&mut self, indent_step_size: usize) {
-        self.indent_step_size = indent_step_size;
-    }
+    fn set_indent_step_size(&mut self, indent_step_size: usize) { self.core.set_indent_step_size(indent_step_size); }
 }
 
 
@@ -125,28 +79,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_new_n_clear() {
-        let mut wr = HTMLWriter::new();
-        assert_eq!(wr.content, "");
-        assert_eq!(wr.indent_step_size, 4);
-        assert_eq!(wr.indent, "");
-        assert_eq!(wr.block_stack, Vec::<String>::new());
-
-        wr.w_open_element("div");
-        wr.set_indent_step(4);
-        wr.set_indent_step_size(8);
-        wr.clear();
-        assert_eq!(wr.content, "");
-        assert_eq!(wr.indent_step_size, 4);
-        assert_eq!(wr.indent, "");
-        assert_eq!(wr.block_stack, Vec::<String>::new());
-    }
-
-    #[test]
     fn test_single_element() {
         let mut wr = HTMLWriter::new();
         wr.w_single_element("img");
-        assert_eq!(wr.content, "<img>".to_string());
+        assert_eq!(wr.core.content, "<img>".to_string());
     }
 
     #[test]
@@ -154,7 +90,7 @@ mod tests {
         let mut wr = HTMLWriter::new();
         wr.w_open_element("div");
         wr.w_close_element();
-        assert_eq!(wr.content, "<div></div>".to_string());
+        assert_eq!(wr.core.content, "<div></div>".to_string());
     }
 
     #[test]
@@ -167,7 +103,7 @@ mod tests {
         wr.w_property("style", "width: auto");
         wr.w_lf_dec();
         wr.w_close_element();
-        assert_eq!(wr.content, "<div class=\"container\">\n    <img style=\"width: auto\">\n</div>")
+        assert_eq!(wr.core.content, "<div class=\"container\">\n    <img style=\"width: auto\">\n</div>")
     }
 
     #[test]
@@ -177,30 +113,11 @@ mod tests {
         let mut wr = HTMLWriter::new();
         wr.w_single_element("img");
         wr.w_properties(&properties);
-        assert_eq!(wr.content, "<img class=\"container\" style=\"width: auto\">".to_string());
+        assert_eq!(wr.core.content, "<img class=\"container\" style=\"width: auto\">".to_string());
 
         wr.clear();
         wr.w_single_element("img");
         wr.w_property("style", "width: auto");
-        assert_eq!(wr.content, "<img style=\"width: auto\">");
-    }
-
-    #[test]
-    fn test_indent_methods() {
-        let mut wr = HTMLWriter::new();
-        assert_eq!(wr.indent, "".to_string());
-
-        wr.set_indent_step(2);
-        assert_eq!(wr.indent, "        ".to_string());
-
-        wr.dec_indent_step();
-        assert_eq!(wr.indent, "    ".to_string());
-
-        wr.inc_indent_step();
-        assert_eq!(wr.indent, "        ".to_string());
-
-        wr.set_indent_step_size(3);
-        wr.set_indent_step(1);
-        assert_eq!(wr.indent, "   ");
+        assert_eq!(wr.core.content, "<img style=\"width: auto\">");
     }
 }
--- src/writer.rs
+++ src/writer.rs
@@ -52,55 +52,118 @@ pub trait MLLWriter {
 }
 
 
-/// The typical Writer struct, to be used to fill the content-string with HTML, XML or JSON.
-/// To be implemented in each variant in a module. Have a look into the html-module.
-/// #[derive(Debug, Clone)]
-/// pub struct Writer {
-///     // holds the whole file content as long the Writer is used
-///     pub content: String,
-///     // number of whitespaces one indent-step means
-///     indent_step_size: usize,
-///     // holds the current indent as a string for quick adding into content
-///     indent: String,
-///     // holds a stack with opened/unclosed block-tags
-///     block_stack: Vec<String>
-/// }
-
-
 /// The Property struct simplifies to encapsule several properties, e.g. class="superhero" and style="width: auto". These can
 /// be passed to the Writer, which pushes it onto the content-string in the right way
-pub struct Property(Vec<(String,String)>);
+pub struct Property {
+    pub(crate) p: Vec<(String,String)>
+}
 
 impl Property {
     /// A default new method with one first property pair to be passed
     pub fn new(name: &str, value: &str) -> Property {
-        let mut p = Property(Vec::new());
-        p.0.push((name.to_string(), value.to_string()));
+        let mut p = Property{ p: Vec::new() };
+        p.p.push((name.to_string(), value.to_string()));
         p
     }
 
     /// Simple method to add other properties to the stack
     pub fn add(&mut self, name: &str, value: &str) {
-        self.0.push((name.to_string(), value.to_string()));
+        self.p.push((name.to_string(), value.to_string()));
+    }
+}
+
+
+/// The typical Writer struct, to be used to fill the content-string with HTML, XML or JSON.
+/// To be implemented in each variant in a module. Have a look into the html-module.
+#[derive(Debug, Clone)]
+pub struct WriterCore {
+    // holds the whole file content as long the Writer is used
+    pub content: String,
+    // number of whitespaces one indent-step means
+    pub(crate) indent_step_size: usize,
+    // holds the current indent as a string for quick adding into content
+    pub(crate) indent: String,
+    // holds a stack with opened/unclosed block-tags
+    pub(crate) block_stack: Vec<String>
+}
+
+impl MLLWriter for WriterCore {
+    type MLLWriter = WriterCore;
+
+    // Methods to be implemented by each subtype individually
+    fn new() -> WriterCore {
+        WriterCore{
+            content: String::new(),
+            indent_step_size: 4,
+            indent: String::new(),
+            block_stack: Vec::new(),
+        }
     }
 
-    /// Method generates the HTML-String variant
-    pub fn html_str(&self) -> String {
-        let mut s = String::new();
-        self.0.iter().for_each(|x| s.push_str(
-            &(" ".to_string() + &x.0 + "=\"" + &x.1 + "\"")
-        ));
-        s
+    fn w_open_element(&mut self, _tag: &str) {
+        // Nothing, because each variant does it in its own way
     }
 
-    /// Method generates the XML-String variant
-    pub fn xml_str(&self) -> String {
-        String::new()
+    fn w_close_element(&mut self) {
+        // Nothing, because each variant does it in its own way
     }
 
-    /// Method generates the JSON-String variant
-    pub fn json_str(&self) -> String {
-        String::new()
+    fn w_single_element(&mut self, _tag: &str) {
+        // Nothing, because each variant does it in its own way
+    }
+
+    fn w_property(&mut self, _name: &str, _value: &str) {
+        // Nothing, because each variant does it in its own way
+    }
+
+    fn w_properties(&mut self, _properties: &Property) {
+        // Nothing, because each variant does it in its own way
+    }
+
+    // Default implementations, can propably reused by every subtype
+    fn clear(&mut self) {
+        self.content.clear();
+        self.set_indent_step(0);
+        self.set_indent_step_size(4);
+        self.block_stack.clear();
+    }
+
+    fn w_lf(&mut self) {
+        self.content.push_str(&["\n".to_string() + &self.indent].concat());
+    }
+
+    fn w_lf_inc(&mut self) {
+        self.inc_indent_step();
+        self.w_lf();
+    }
+
+    fn w_lf_dec(&mut self) {
+        self.dec_indent_step();
+        self.w_lf();
+    }
+
+    fn inc_indent_step(&mut self) {
+        self.indent.push_str(" ".repeat(self.indent_step_size).as_str());
+    }
+
+    fn dec_indent_step(&mut self) {
+        let steps = if self.indent_step_size > self.indent.len() { 
+            self.indent.len() 
+        }
+        else { 
+            self.indent_step_size 
+        };
+        for _i in 0..steps {
+            self.indent.pop();
+        }
+    }
+
+    fn set_indent_step(&mut self, indent_step: usize) {
+        self.indent = " ".repeat(indent_step * self.indent_step_size).to_string();
+    }
+
+    fn set_indent_step_size(&mut self, indent_step_size: usize) {
+        self.indent_step_size = indent_step_size;
     }
 }
 
@@ -113,9 +176,46 @@ mod tests {
     #[test]
     fn test_property() {
         let mut prop = Property::new("class", "superhero");
-        assert_eq!(prop.0[0], ("class".to_string(), "superhero".to_string()));
+        assert_eq!(prop.p[0], ("class".to_string(), "superhero".to_string()));
 
         prop.add("style", "width: auto");
-        assert_eq!(prop.0[1], ("style".to_string(), "width: auto".to_string()));
+        assert_eq!(prop.p[1], ("style".to_string(), "width: auto".to_string()));
+    }
+
+    #[test]
+    fn test_new_n_clear() {
+        let mut wr = WriterCore::new();
+        assert_eq!(wr.content, "");
+        assert_eq!(wr.indent_step_size, 4);
+        assert_eq!(wr.indent, "");
+        assert_eq!(wr.block_stack, Vec::<String>::new());
+
+        wr.w_open_element("div");
+        wr.set_indent_step(4);
+        wr.set_indent_step_size(8);
+        wr.clear();
+        assert_eq!(wr.content, "");
+        assert_eq!(wr.indent_step_size, 4);
+        assert_eq!(wr.indent, "");
+        assert_eq!(wr.block_stack, Vec::<String>::new());
+    }
+
+    #[test]
+    fn test_indent_methods() {
+        let mut wr = WriterCore::new();
+        assert_eq!(wr.indent, "".to_string());
+
+        wr.set_indent_step(2);
+        assert_eq!(wr.indent, "        ".to_string());
+
+        wr.dec_indent_step();
+        assert_eq!(wr.indent, "    ".to_string());
+
+        wr.inc_indent_step();
+        assert_eq!(wr.indent, "        ".to_string());
+
+        wr.set_indent_step_size(3);
+        wr.set_indent_step(1);
+        assert_eq!(wr.indent, "   ");
     }
 }
